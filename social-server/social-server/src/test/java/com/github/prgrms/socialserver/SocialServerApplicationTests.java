04f2ecdfa485e2c305085f2c4f700bc909681461
LeeMyoungJin <audwls6372@gmail.com>
2020-01-24 18:33:38 +0900

    test

---
+++ social-server/social-server/src/test/java/com/github/prgrms/socialserver/SocialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prgrms.socialserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
